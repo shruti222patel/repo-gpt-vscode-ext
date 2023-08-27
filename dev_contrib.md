18811beb118b8bdaac9ea7ea0b497eb4c1050316
Shruti Patel <shruti222patel@gmail.com>
2023-08-27 14:38:52 -0400

    The explain now works with the chat interface

--- dev_contrib.md
+++ dev_contrib.md
@@ -5,3 +5,8 @@
 
 
 If you've followed the above steps and are still facing the issue, it could be helpful to see if there's any detailed logging available. Sometimes, VS Code provides more specific errors in its console. You can access this by pressing Ctrl+Shift+U (or Cmd+Shift+U on macOS) to bring up the Output panel and selecting "Log (Extension Host)" from the dropdown.
+
+
+## Helpful Resources
+- [Create Q&A chatbot](https://community.openai.com/t/context-generation-for-chat-based-q-a-bot/103121/18)
+- [Example chatbot vscode ext ui](https://github.com/barnesoir/chatgpt-vscode-plugin)
--- src/extension.ts
+++ src/extension.ts
@@ -7,14 +7,19 @@ const path = require('path');
 import { spawn } from 'child_process';
 import { create } from 'domain';
 
+
+import ChatGptViewProvider from './chatgpt-view-provider';
+
+
 function setupPythonEnv(context: vscode.ExtensionContext) {
     const extensionDir = context.extensionPath;
     const venvDir = path.join(extensionDir, 'venv');
     const wheelPath = path.join(extensionDir, 'repo_gpt-0.1.6-py3-none-any.whl');
     const lastWheelPath = path.join(extensionDir, 'last_wheel.txt');
+    const pythonInterpreter = path.join(venvDir, 'bin', 'python');
 
     // If last_wheel.txt doesn't exist or contains a different wheel filename, recreate venv
-    if (!fs.existsSync(venvDir) || !fs.existsSync(lastWheelPath) || fs.readFileSync(lastWheelPath, 'utf8') !== path.basename(wheelPath)) {
+    if (!pythonInterpreter || !fs.existsSync(lastWheelPath) || fs.readFileSync(lastWheelPath, 'utf8') !== path.basename(wheelPath)) {
         // Remove existing venv directory if it exists
         if (fs.existsSync(venvDir)) {
             fs.rmdirSync(venvDir, { recursive: true });
@@ -30,7 +35,7 @@ function setupPythonEnv(context: vscode.ExtensionContext) {
         fs.writeFileSync(lastWheelPath, path.basename(wheelPath));
     }
 
-    return path.join(venvDir, 'bin', 'python');
+    return pythonInterpreter;
 }
 
 
@@ -61,18 +66,23 @@ export function activate(context: vscode.ExtensionContext) {
 
     const pythonInterpreter = setupPythonEnv(context);
 
+
+    // REGISTER SUBSCRIOTIONS
+    // Register ChatGptViewProvider for all languages
+    const chatViewProvider = new ChatGptViewProvider(context);
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider("repogpt.view", chatViewProvider, {
+        webviewOptions: { retainContextWhenHidden: true }
+    }));
+    console.log("registered webview view provider");
+
 	// Register FunctionRunCodeLensProvider for all languages
 	const languages = ['typescript', 'php', 'python', 'sql'];
-
     for (const lang of languages) {
         context.subscriptions.push(vscode.languages.registerCodeLensProvider(
             { scheme: 'file', language: lang },
             new FunctionRunCodeLensProvider(lang, context)
         ));
     }
-
-    // Create a map to store the state for each function
-    const functionStates: { [key: string]: string } = {};
 
     context.subscriptions.push(vscode.commands.registerCommand('repogpt.createTest', async (functionBody: string, originalFilePath: string, language: string) => {
     
@@ -126,133 +136,22 @@ export function activate(context: vscode.ExtensionContext) {
 
         // Construct the command
         const pythonProcess = spawn(pythonInterpreter, [pythonScriptPath, apiKey, language, tempFilePath]);
-        // Create a webview panel to stream Python script output
-        const panel = vscode.window.createWebviewPanel(
-            'pythonScriptOutput',      // Identifies the type of the webview
-            `Explain - ${functionName}`,   // Title of the panel displayed to the user
-            vscode.ViewColumn.Beside,    // Determines the column to show the new webview
-            { 
-                enableScripts: true,   // Enables JavaScript in the webview
-                retainContextWhenHidden: true  // Retains the webview context when hidden
-            }
-        );
 
-        // After creating the webview panel, move it to the last editor group (usually the bottom panel)
-        vscode.commands.executeCommand('workbench.action.moveEditorToLastGroup');
-
-
-        // Create a unique key for the function. Here we're just using the function name,
-        // but you may need to make this more unique if there are potential overlaps.
-        const functionKey = functionName;
-        // Get the saved state for the function if available
-        let savedOutput = functionStates[functionKey] || '';
-
-        // Get the editor.fontFamily setting
-        let editorFont = vscode.workspace.getConfiguration('editor').get('fontFamily');
-
-        // Initial HTML structure with a script to handle messages from the extension
-        let initialHtml = `
-        <html>
-            <head>
-                <style>
-                body {
-                    font-family: "${editorFont}", monospace;
-                    padding: 16px;
-                    line-height: 1.6;
-                    white-space: pre-wrap;
-                }
-                h1, h2, h3, h4, h5, h6 {
-                    margin-top: 24px;
-                    margin-bottom: 16px;
-                }
-                h1 {
-                    font-size: 2em;
-                }
-                h2 {
-                    font-size: 1.5em;
-                }
-                h3 {
-                    font-size: 1.25em;
-                }
-                h4 {
-                    font-size: 1em;
-                }
-                h5 {
-                    font-size: 0.875em;
-                }
-                h6 {
-                    font-size: 0.85em;
-                    color: #777;
-                }
-                a {
-                    color: #0366d6;
-                    text-decoration: none;
-                }
-                a:hover {
-                    text-decoration: underline;
-                }
-                blockquote {
-                    color: #777;
-                    border-left: 4px solid #ddd;
-                    padding-left: 10px;
-                    margin-left: 0;
-                }
-                ul, ol {
-                    margin-top: 0;
-                    margin-bottom: 16px;
-                    padding-left: 40px;
-                }
-                code {
-                    padding: 2px 5px;
-                    background-color: #f2f2f2;
-                    border-radius: 3px;
-                }
-                pre {
-                    background-color: #f2f2f2;
-                    padding: 10px;
-                    border-radius: 3px;
-                    white-space: pre-wrap;
-                }
-                </style>
-            </head>
-            <body>
-                <div id="output">${savedOutput}</div>
-                <script>
-                    const outputPre = document.getElementById('output');
-                    window.addEventListener('message', event => {
-                        const message = event.data;
-                        switch (message.type) {
-                            case 'append':
-                                if (message.isError) {
-                                    outputPre.style.color = 'red';
-                                }
-                                outputPre.textContent += message.data;
-                                break;
-                        }
-                    });
-                </script>
-            </body>
-        </html>
-        `;
-
-        panel.webview.html = initialHtml;
-
-        panel.onDidDispose(() => {
-            functionStates[functionKey] = panel.webview.html; // save the state for the function when the webview is disposed
-        });
+        // Create new response html
+        chatViewProvider.sendMessageToWebView({ type: 'addResponse', value: `Explaination of: ${functionName}` });
 
         pythonProcess.stdout.on('data', (data) => {
-            savedOutput += data.toString();
-            panel.webview.postMessage({ type: 'append', data: data.toString(), isError: false });
+            chatViewProvider.sendMessageToWebView({ type: 'append', value: data.toString(), isError: false });
         });
 
         pythonProcess.stderr.on('data', (data) => {
-            panel.webview.postMessage({ type: 'append', data: data.toString(), isError: true });
+            // console.error(data.toString());
+            chatViewProvider.sendMessageToWebView({ type: 'append', value: data.toString(), isError: true });
         });
 
-        pythonProcess.on('close', (code) => {
-            panel.webview.postMessage({ type: 'append', data: `Python script exited with code ${code}`, isError: false });
-        });
+        // pythonProcess.on('close', (code) => {
+        //     chatViewProvider.sendMessageToWebView({ type: 'append', value: `Python script exited with code ${code}`, isError: false });
+        // });
     }));
 }
 
